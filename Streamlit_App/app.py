866fd15781465675ae542d48764f713da300ce53
mpng1 <120417606+pointertidigital@users.noreply.github.com>
2024-01-12 14:16:00 -0300

    permitir conversação e renderizar imagens

--- Streamlit_App/app.py
+++ Streamlit_App/app.py
@@ -18,87 +18,60 @@ def crop_to_circle(image):
     result = ImageOps.fit(image, mask.size, centering=(0.5, 0.5))
     result.putalpha(mask)
     return result
+    
+# Function to check if the string is a link to an image
+def is_image_link(text):
+    image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg', '.webp']
+    return any(text.lower().endswith(ext) for ext in image_extensions)
+
+# Function to render images in the answer
+def render_images(answer):
+    lines = answer.split('\n')
+    for i, line in enumerate(lines):
+        if is_image_link(line):
+            st.image(line, caption=f"Image {i+1}", use_column_width=True)
+
 
 # Title
 st.title("Assistente Pointer")
-
-# Display a text box for input
-prompt = st.text_input("Em que posso ajudar?", max_chars=2000)
-prompt = prompt.strip()
-
-# Display a primary button for submission
-submit_button = st.button("Enviar", type="primary")
-
-# Button to reset session
-#reset_button = st.button("Resetar Sessão")
-
-# Display a button to end the session
-#end_session_button = st.button("Finalizar sessão")
-
-# Sidebar for user input
-st.sidebar.title("Processamento")
-
-
-def filter_trace_data(trace_data, query):
-    if query:
-        # Filter lines that contain the query
-        return "\n".join([line for line in trace_data.split('\n') if query.lower() in line.lower()])
-    return trace_data
-    
-    
 
 # Session State Management
 if 'session_id' not in st.session_state:
     st.session_state['session_id'] = sessionid
     st.session_state['history'] = []
 
-# Function to reset session
-def reset_session():
-    st.session_state['session_id'] = sessionid
-    st.session_state['history'] = []
-    
-# Reset session if the button is clicked
-#if reset_button:
-#    reset_session()
+while True:
+    # Display a text box for input
+    prompt = st.text_input("Em que posso ajudar?", max_chars=2000)
+    prompt = prompt.strip()
 
+    # Display a primary button for submission
+    submit_button = st.button("Enviar", type="primary")
 
-# Function to parse and format response
-def format_response(response_body):
-    try:
-        # Try to load the response as JSON
-        data = json.loads(response_body)
-        # If it's a list, convert it to a DataFrame for better visualization
-        if isinstance(data, list):
-            return pd.DataFrame(data)
-        else:
-            return response_body
-    except json.JSONDecodeError:
-        # If response is not JSON, return as is
-        return response_body
+    if not submit_button or not prompt:
+        break  # Break the loop if the user didn't submit a question
 
+    # Function to reset session
+    def reset_session():
+        st.session_state['session_id'] = sessionid
+        st.session_state['history'] = []
 
-
-# Handling user input and responses
-if submit_button and prompt:
+    # Handling user input and responses
     event = {
         "sessionId": sessionid,
         "question": prompt
     }
     response = agenthelper.lambda_handler(event, None)
-    
+
     # Parse the JSON string
     response_data = json.loads(response['body'])
-    print("TRACE & RESPONSE DATA ->  ", json.loads(response['body']))
-    
+
     # Check if 'response' key is present in response_data
     if 'response' in response_data:
         # Extract the response and trace data
         all_data = format_response(response_data['response'])
         the_response = response_data['trace_data']
 
-        # Clear the conversation history before appending new items
-        st.session_state['history'] = []
-        
         # Append the current question and answer to the conversation history
         st.session_state['history'].append({"question": prompt, "answer": the_response})
 
@@ -107,25 +80,12 @@ if submit_button and prompt:
         st.session_state['trace_data'] = the_response
     else:
         # Handle the case when 'response' key is not present
-        st.sidebar.text("Error: 'response' key not found in the API response.")   
-    
-
-#if end_session_button:
-#    st.session_state['history'].append({"question": "Session Ended", "answer": #"Obrigado por ajudar a testar o Assistente Pointer. :)"})
-#    event = {
-#        "sessionId": sessionid,
-#        "question": "placeholder to end session",
-#        "endSession": True
-#    }
-#    agenthelper.lambda_handler(event, None)
-#    st.session_state['history'].clear()
-
+        st.sidebar.text("Error: 'response' key not found in the API response.")
 
 # Display conversation history
 st.write("## Output")
 
 for chat in reversed(st.session_state['history']):
-    
     # Creating columns for Question
     col1_q, col2_q = st.columns([2, 10])
     with col1_q:
@@ -133,7 +93,7 @@ for chat in reversed(st.session_state['history']):
         circular_human_image = crop_to_circle(human_image)
         st.image(circular_human_image, width=125)
     with col2_q:
-        st.text_area("Q:", value=chat["question"], height=50, key=str(chat)+"q", disabled=True)
+        st.text_area("Q:", value=chat["question"], height=50, key=str(chat) + "q", disabled=True)
 
     # Creating columns for Answer
     col1_a, col2_a = st.columns([2, 10])
@@ -150,5 +110,5 @@ for chat in reversed(st.session_state['history']):
             circular_robot_image = crop_to_circle(robot_image)
             st.image(circular_robot_image, width=150)
         with col2_a:
-            st.text_area("A:", value=chat["answer"], height=100, key=str(chat)+"a")
-
+            st.text_area("A:", value=chat["answer"], height=100, key=str(chat) + "a")
+            render_images(chat["answer"])
